fbf5b69b44bb03c2eea0859e034ef0fa33ceab1d
Denys <none@none>
2014-04-29 22:05:21 +0200

    missing files... this joke never gets old

---
+++ D3Data/KillStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3Data
+{
+    public class KillStats
+    {
+        public int monsters { get; set; }
+        public int hardcoreMonsters { get; set; }
+        public int elites { get; set; }
+    }
+}
---
+++ D3HeroesTool.Test/DeserializeKillStatsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3HeroesTool.Test
+{
+    [TestFixture]
+    public class DeserializeKillStatsTests
+    {
+        [Test]
+        public void monsters()
+        {
+            D3Data.Career c = D3Data.Deserializer.AsCareer("{ 'kills': { 'monsters':1764 } }");
+            Assert.AreEqual(1764, c.kills.monsters);
+        }
+
+        [Test]
+        public void hardcoreMonsters()
+        {
+            D3Data.Career c = D3Data.Deserializer.AsCareer("{ 'kills': { 'hardcoreMonsters':6 } }");
+            Assert.AreEqual(6, c.kills.hardcoreMonsters);
+        }
+
+        [Test]
+        public void elites()
+        {
+            D3Data.Career c = D3Data.Deserializer.AsCareer("{ 'kills': { 'elites':42 } }");
+            Assert.AreEqual(42, c.kills.elites);
+        }
+    }
+}
