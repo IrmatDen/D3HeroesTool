44567d26a38a5f97a04c92d0ebca0e3e91271e5a
Denys <none@none>
2014-05-01 00:06:05 +0200

    Added IBiMap and SimpleBiMap<,> (provides Add(,), TryGetByLeft, TryGetByRight)

---
+++ D3HeroesTool.Test/SimpleBiMapTests.cs
@@ -0,0 +1,80 @@
+﻿using D3HeroesTool.Utils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3HeroesTool.Test
+{
+    [TestFixture]
+    class SimpleBiMapTests
+    {
+        [Test]
+        public void Test_Count_1()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+            Assert.AreEqual(1, bm.Count);
+        }
+
+        [Test]
+        public void Test_Count_2()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+            bm.Add("answer²", 42*42);
+            Assert.AreEqual(2, bm.Count);
+        }
+
+        [Test]
+        public void Test_ByLeft()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+
+            int theAnswer;
+            Assert.AreEqual(true, bm.TryGetByLeft("answer", out theAnswer));
+        }
+
+        [Test]
+        public void Test_ByRight()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+
+            string theQuestion;
+            Assert.AreEqual(true, bm.TryGetByRight(42, out theQuestion));
+        }
+
+        [Test]
+        public void Test_FailedAdd()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+            Assert.Throws<ArgumentException>(() => { bm.Add("answer", 42); });
+            Assert.AreEqual(1, bm.Count);
+        }
+
+        [Test]
+        public void Test_Failed_ByLeft()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+
+            int theAnswer;
+            Assert.AreEqual(false, bm.TryGetByLeft("foo", out theAnswer));
+        }
+
+        [Test]
+        public void Test_Failed_ByRight()
+        {
+            SimpleBiMap<string, int> bm = new SimpleBiMap<string, int>();
+            bm.Add("answer", 42);
+
+            string theQuestion;
+            Assert.AreEqual(false, bm.TryGetByRight(21, out theQuestion));
+        }
+    }
+}
---
+++ D3HeroesTool/Utils/SimpleBiMap.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3HeroesTool.Utils
+{
+    /// <summary>
+    /// Doesn't feel as right as templated interface, but makes it usable from reflection code
+    /// </summary>
+    public interface IBiMap
+    {
+        void Add(object left, object right);
+        bool TryGetByLeft(object leftKey, out object rightValue);
+        bool TryGetByRight(object rightKey, out object leftValue);
+    }
+
+    public class SimpleBiMap<LeftType, RightType> : IBiMap
+    {
+        IDictionary<LeftType, RightType> leftToRight = new Dictionary<LeftType, RightType>();
+        IDictionary<RightType, LeftType> rightToLeft = new Dictionary<RightType, LeftType>();
+
+        public int Count { get { return leftToRight.Count; } }
+
+        public void Add(LeftType left, RightType right)
+        {
+            if (leftToRight.ContainsKey(left))
+                throw new ArgumentException(String.Format("{0} already registered as left key", left.ToString()));
+            if (rightToLeft.ContainsKey(right))
+                throw new ArgumentException(String.Format("{0} already registered as right key", right.ToString()));
+
+            leftToRight.Add(left, right);
+            try
+            {
+                rightToLeft.Add(right, left);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    leftToRight.Remove(left);
+                }
+                catch (Exception nestedEx)
+                {
+                    throw new Exception("Couldn't cleanup after failure from reverse dictionary. Structure corrupted",
+                        nestedEx);
+                }
+                throw ex;
+            }
+        }
+
+        public bool TryGetByLeft(LeftType k, out RightType v)
+        {
+            return leftToRight.TryGetValue(k, out v);
+        }
+
+        public bool TryGetByRight(RightType k, out LeftType v)
+        {
+            return rightToLeft.TryGetValue(k, out v);
+        }
+
+        void IBiMap.Add(object left, object right)
+        {
+            Add((LeftType)left, (RightType)right);
+        }
+
+        bool IBiMap.TryGetByLeft(object leftKey, out object rightValue)
+        {
+            RightType wanted;
+            bool res = TryGetByLeft((LeftType)leftKey, out wanted);
+            rightValue = wanted;
+            return res;
+        }
+
+        bool IBiMap.TryGetByRight(object rightKey, out object leftValue)
+        {
+            LeftType wanted;
+            bool res = TryGetByRight((RightType)rightKey, out wanted);
+            leftValue = wanted;
+            return res;
+        }
+    }
+}
