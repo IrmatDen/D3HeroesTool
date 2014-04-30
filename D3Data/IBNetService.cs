67a76cf21675e0f9bd369240122c5c8e1a1b6b2d
Denys <none@none>
2014-04-30 22:23:39 +0200

    Moved sample jsons around
    --HG--
    rename : D3HeroesTool.Test/sample_json/hero.json => sample_jsons/6174823.json
    rename : D3HeroesTool.Test/sample_json/flawless_royal_emeral.json => sample_jsons/flawless_royal_emeral.json
    rename : D3HeroesTool.Test/sample_json/generic_helm.json => sample_jsons/generic_helm.json
    rename : D3HeroesTool.Test/sample_json/career.json => sample_jsons/irmatden-2108.json
    rename : D3HeroesTool.Test/sample_json/set_pant_crimson.json => sample_jsons/set_pant_crimson.json
    rename : D3HeroesTool.Test/sample_json/wpn_solanium.json => sample_jsons/wpn_solanium.json

--- D3Data/IBNetService.cs
+++ D3Data/IBNetService.cs
@@ -6,12 +6,40 @@ using System.Threading.Tasks;
 
 namespace D3Data
 {
-    public enum Server { us, eu, tw, kr, cn };
-    public enum Locale { en_US, en_GB, es_MX, es_ES, it_IT, pt_PT, fr_FR, de_DE, pl_PL, ru_RU, ko_KR, zh_TW, zh_CN };
+    public enum Server
+    {
+        [Attributes.DisplayString(ResourceKey="D3Server_US")]
+        us,
+        [Attributes.DisplayString(ResourceKey = "D3Server_EU")]
+        eu,
+        [Attributes.DisplayString(ResourceKey = "D3Server_TW")]
+        tw,
+        [Attributes.DisplayString(ResourceKey = "D3Server_KR")]
+        kr,
+        [Attributes.DisplayString(ResourceKey = "D3Server_CN")]
+        cn
+    }
+
+    public enum Locale
+    {
+        en_US,
+        en_GB,
+        es_MX,
+        es_ES,
+        it_IT,
+        pt_PT,
+        fr_FR,
+        de_DE,
+        pl_PL,
+        ru_RU,
+        ko_KR,
+        zh_TW,
+        zh_CN
+    };
 
     public interface IBNetService
     {
         void Setup(Server s, string battleTag, Locale l = Locale.en_US);
-        Task GetCareer(Action<string> onCareerJSonReceived, Action onError);
+        void GetCareer(Action<string> onCareerJSonReceived, Action onError);
     }
 }
--- D3HeroesTool/App.xaml.cs
+++ D3HeroesTool/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,19 @@ namespace D3HeroesTool
     /// </summary>
     public partial class App : Application
     {
+        public D3Data.IBNetService ActiveBNet;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Any(arg => arg == "--filesystem"))
+            {
+                FSBNetService serv = new FSBNetService();
+                serv.RootFolder = Directory.GetCurrentDirectory() + "\\sample_jsons";
+                if (!Directory.Exists(serv.RootFolder))
+                    throw new Exception("--filesystem can only be used from a folder containing the sample_jsons subfolder");
+
+                ActiveBNet = serv;
+            }
+        }
     }
 }
