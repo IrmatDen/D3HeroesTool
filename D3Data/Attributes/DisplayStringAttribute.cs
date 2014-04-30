e0e6b7c5e474af39e4f705e976b0b152404ae791
Denys <none@none>
2014-04-30 22:25:15 +0200

    Added DisplayString annotation to prepare binding of enum to UI comboboxes

---
+++ D3Data/Attributes/DisplayStringAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class DisplayStringAttribute : Attribute
+    {
+        private readonly string value;
+        public string Value { get { return value; } }
+
+        public string ResourceKey { get; set; }
+
+        public DisplayStringAttribute() { }
+        public DisplayStringAttribute(string v) { value = v; }
+    }
+}
