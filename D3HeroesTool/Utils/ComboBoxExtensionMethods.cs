246e5c2fab13679a042a508fcb7f08f08b7721cd
Denys <none@none>
2014-05-01 16:03:14 +0200

    Server & Language comboboxes now match their width to that of their biggest item

---
+++ D3HeroesTool/Utils/ComboBoxExtensionMethods.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace D3HeroesTool.Utils
+{
+    public static class ComboBoxExtensionMethods
+    {
+        /// <summary>
+        /// This will allow cb's Width to Match the Width of its widest element.
+        /// It does so by triggering these 2 events once:
+        /// <list type="bullet">
+        /// <item>ComboBox.ItemContainerGenerator.StatusChanged</item>
+        /// <item>ComboBox.DropDownOpened</item>
+        /// </list>
+        /// </summary>
+        /// <param name="cb"></param>
+        public static void SetWidthFromItems(this ComboBox cb, bool restrictMinWidth, bool restrictMaxWidth)
+        {
+            double cbWidth = 19; // combobox dropdown button?
+
+            // Get manipulators
+            ComboBoxAutomationPeer peer = new ComboBoxAutomationPeer(cb);
+            IExpandCollapseProvider provider = (IExpandCollapseProvider)peer.GetPattern(PatternInterface.ExpandCollapse);
+
+            // Create an event that will be called once at the end of the function
+            EventHandler evtHandler = null;
+            evtHandler = new EventHandler(
+                (object o, EventArgs args) =>
+                {
+                    if (cb.IsDropDownOpen &&
+                        cb.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
+                    {
+                        double biggestItemWidth = 0;
+                        foreach (var item in cb.Items)
+                        {
+                            // Get item size by allowing it to expand as much as it wants
+                            ComboBoxItem cbItem = cb.ItemContainerGenerator.ContainerFromItem(item) as ComboBoxItem;
+                            cbItem.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                            if (cbItem.DesiredSize.Width > biggestItemWidth)
+                                biggestItemWidth = cbItem.DesiredSize.Width;
+                        }
+
+                        cb.Width = cbWidth + biggestItemWidth;
+                        if (restrictMinWidth)
+                            cb.MinWidth = cb.Width;
+                        if (restrictMaxWidth)
+                            cb.MaxWidth = cb.Width;
+
+                        cb.ItemContainerGenerator.StatusChanged -= evtHandler;
+                        cb.DropDownOpened -= evtHandler;
+                        provider.Collapse();
+                    }
+                });
+
+            cb.ItemContainerGenerator.StatusChanged += evtHandler;
+            cb.DropDownOpened += evtHandler;
+
+            // Trigger our sizer event
+            provider.Expand();
+        }
+    }
+}
---
+++ D3HeroesTool/Utils/ComboBoxWidthFromItemBehavior.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace D3HeroesTool.Utils
+{
+    public static class ComboBoxWidthFromItemBehavior
+    {
+        public static readonly DependencyProperty ComboBoxWidthFromItemProperty =
+            DependencyProperty.RegisterAttached(
+                "ComboBoxWidthFromItems",
+                typeof(bool),
+                typeof(ComboBoxWidthFromItemBehavior),
+                new UIPropertyMetadata(false, OnComboBoxWidthFromItemsPropertyChanged)
+            );
+
+        public static readonly DependencyProperty ComboBoxWidthFromItemRestrictMinWidthProperty =
+            DependencyProperty.RegisterAttached(
+                "ComboBoxRestrictMinWidthFromItems",
+                typeof(bool),
+                typeof(ComboBoxWidthFromItemBehavior),
+                new UIPropertyMetadata(false, OnComboBoxWidthFromItemsPropertyChanged)
+            );
+
+        public static readonly DependencyProperty ComboBoxWidthFromItemRestrictMaxWidthProperty =
+            DependencyProperty.RegisterAttached(
+                "ComboBoxRestrictMaxWidthFromItems",
+                typeof(bool),
+                typeof(ComboBoxWidthFromItemBehavior),
+                new UIPropertyMetadata(false, OnComboBoxWidthFromItemsPropertyChanged)
+            );
+
+        public static bool GetComboBoxWidthFromItems(DependencyObject d)
+        {
+            return (bool)d.GetValue(ComboBoxWidthFromItemProperty);
+        }
+
+        public static void SetComboBoxWidthFromItems(DependencyObject d, bool value)
+        {
+            d.SetValue(ComboBoxWidthFromItemProperty, value);
+        }
+
+        public static bool GetComboBoxRestrictMinWidthFromItems(DependencyObject d)
+        {
+            return (bool)d.GetValue(ComboBoxWidthFromItemRestrictMinWidthProperty);
+        }
+
+        public static void SetComboBoxRestrictMinWidthFromItems(DependencyObject d, bool value)
+        {
+            d.SetValue(ComboBoxWidthFromItemRestrictMinWidthProperty, value);
+        }
+
+        public static bool GetComboBoxRestrictMaxWidthFromItems(DependencyObject d)
+        {
+            return (bool)d.GetValue(ComboBoxWidthFromItemRestrictMaxWidthProperty);
+        }
+
+        public static void SetComboBoxRestrictMaxWidthFromItems(DependencyObject d, bool value)
+        {
+            d.SetValue(ComboBoxWidthFromItemRestrictMaxWidthProperty, value);
+        }
+
+        private static void OnComboBoxWidthFromItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox cb = d as ComboBox;
+            if (cb != null)
+            {
+                if ((bool)e.NewValue)
+                    cb.Loaded += OnComboBoxLoaded;
+                else
+                    cb.Loaded -= OnComboBoxLoaded;
+            }
+        }
+
+        private static void OnComboBoxLoaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            bool restrictMin = GetComboBoxRestrictMinWidthFromItems(cb);
+            bool restrictMax = GetComboBoxRestrictMaxWidthFromItems(cb);
+
+            Action sizeOnIdle = new Action(() => { cb.SetWidthFromItems(restrictMin, restrictMax); });
+            cb.Dispatcher.BeginInvoke(sizeOnIdle, DispatcherPriority.ApplicationIdle);
+        }
+    }
+}
