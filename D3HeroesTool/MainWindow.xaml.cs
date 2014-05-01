cf0323a6634b7c2d0a43b684d3772298e5c643a9
Denys <none@none>
2014-05-01 03:33:21 +0200

    Added ServiceInfo to store/bind server, battletag and locale

--- D3HeroesTool/MainWindow.xaml.cs
+++ D3HeroesTool/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,64 @@ using System.Windows.Shapes;
 
 namespace D3HeroesTool
 {
+    class ServiceInfo : INotifyPropertyChanged
+    {
+        private D3Data.Server _server;
+        private D3Data.Locale _locale;
+        private string _battletag;
+
+        public D3Data.Server Server
+        {
+            get { return _server; }
+            set
+            {
+                _server = value;
+                OnPropertyChanged("Server");
+            }
+        }
+
+        public D3Data.Locale Locale
+        {
+            get { return _locale; }
+            set
+            {
+                _locale = value;
+                OnPropertyChanged("Locale");
+            }
+        }
+
+        public string BattleTag
+        {
+            get { return _battletag; }
+            set
+            {
+                _battletag = value;
+                OnPropertyChanged("BattleTag");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceInfo si = new ServiceInfo();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            pnlServiceInfo.DataContext = si;
+            tbDebug.DataContext = si;
         }
     }
 }
