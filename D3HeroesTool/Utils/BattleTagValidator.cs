6317bd2192e322b1b4543711cc5838cd2e4aefcd
Denys <none@none>
2014-05-01 04:20:56 +0200

    Added BattleTag syntaxic validation

---
+++ D3HeroesTool/Utils/BattleTagValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace D3HeroesTool.Utils
+{
+    public class BattleTagValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string[] elems = ((string)value).Split('#');
+            if (elems.Length != 2)
+                return new ValidationResult(false, "A valid BattleTag includes one, and only one, '#' (ex: MyNickname#1234)");
+
+            try
+            {
+                int integral_part = int.Parse(elems[1]);
+            }
+            catch // Expect FormatException
+            {
+                return new ValidationResult(false,
+                        "A valid BattleTag contains a number after '#' (ex: MyNickname#1234) instead of " + elems[1]);
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ D3HeroesTool/Utils/ValidationErrorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace D3HeroesTool.Utils
+{
+    public class ValidationErrorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var err = value as ValidationError;
+            if (err != null)
+                return err.ErrorContent;
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
