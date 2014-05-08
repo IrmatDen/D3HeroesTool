724a61b0ff969d696cf4ae027514117524946ead
Denys <none@none>
2014-05-08 16:46:03 +0200

    HeroRefConverter (used to match the lastHeroPlayed against the career's heroes)

---
+++ D3Data/Converters/HeroRefConverter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace D3Data.Converters
+{
+    class HeroRefConverter : JsonConverter
+    {
+        public static Career careerRef { get; set; }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.Integer)
+            {
+                throw new Exception(String.Format("Unexpected token parsing date. Expected Integer, got {0}.",
+                                                  reader.TokenType));
+            }
+
+            var id = (long)reader.Value;
+            return careerRef.heroes.First(h => h.id == id);
+        }
+
+        /// <summary>
+        /// We won't write json, so no need to provide a meaningul implementation, right?
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="serializer"></param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(HeroSummary);
+        }
+    }
+}
