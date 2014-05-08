a9f738c39cc21a7ad44a793ade6944b867bc3fe0
Denys <none@none>
2014-05-08 20:57:19 +0200

    Added HeroNameLevelConverter to replace HeroSummary.NameLevel property (keeps the model clean) Moved EnumDisplayers & ValidationErrorConverter resources from application-wide to main window only

---
+++ D3HeroesTool/Utils/HeroNameLevelConverter.cs
@@ -0,0 +1,27 @@
+﻿using D3Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace D3HeroesTool.Utils
+{
+    public class HeroNameLevelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var hero = value as HeroSummary;
+            if (hero != null)
+                return String.Format("{0} ({1})", hero.name, hero.level.ToString());
+            return "#CONVERT_FAILED#";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
