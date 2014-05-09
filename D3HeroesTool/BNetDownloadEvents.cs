89da8da85abf5caff383445bf731edb4a8741777
Denys <none@none>
2014-05-10 00:14:07 +0200

    Added download events

---
+++ D3HeroesTool/BNetDownloadEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3HeroesTool
+{
+    public class BNetDownloadStartedEventArgs : EventArgs
+    {
+        string _downloadType;
+
+        public string DownloadType { get { return _downloadType; } }
+
+        public BNetDownloadStartedEventArgs(string downloadType)
+        {
+            _downloadType = downloadType;
+        }
+    }
+    public delegate void DownloadStartedEventHandler(object sender, BNetDownloadStartedEventArgs e);
+
+    public class BNetDownloadFinishedEventArgs : EventArgs
+    {
+        public BNetDownloadFinishedEventArgs()
+        {
+        }
+    }
+    public delegate void DownloadFinishedEventHandler(object sender, BNetDownloadFinishedEventArgs e);
+}
