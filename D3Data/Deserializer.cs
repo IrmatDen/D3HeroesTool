f57fd3a185fddcf5c374938fa794c9cac6742578
IrmatDen <denys.bulant@bbox.fr>
2014-05-18 17:02:21 +0200

    [comments]

--- D3Data/Deserializer.cs
+++ D3Data/Deserializer.cs
@@ -8,6 +8,11 @@ namespace D3Data
     /// </summary>
     public class Deserializer
     {
+        /// <summary>
+        /// Converts (or try) some JSon into a Diablo 3 career
+        /// </summary>
+        /// <param name="json">json representing the career</param>
+        /// <returns>newly loaded career</returns>
         public static Career AsCareer(string json)
         {
             Career career = new Career();
@@ -17,7 +22,16 @@ namespace D3Data
             return career;
         }
 
+        /// <summary>
+        /// Caches the list of hero we already loaded (to not re-populate them)
+        /// </summary>
         private static HashSet<Hero> alreadyLoadedHeroes = new HashSet<Hero>();
+
+        /// <summary>
+        /// Finish loading the given hero based on provided JSon data (or no-op if the hero was already loaded)
+        /// </summary>
+        /// <param name="json">expected to contain the hero parameters</param>
+        /// <param name="target">the hero which we want to finish loading</param>
         public static void CompleteHero(string json, Hero target)
         {
             if (alreadyLoadedHeroes.Contains(target))
